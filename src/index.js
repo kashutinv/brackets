c4081350d19fa7696f2261bd136920a3866da5b7
Vasyl Kashutin <vasyl.kashutin@gmail.com>
2021-02-23 21:01:36 +0200

    Implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 1) {
+    return false;
+  }
+
+  let openBrackets = [];
+  let closedBrackets = [];
+
+  for (bracketsSet of bracketsConfig) {
+    openBrackets.push(bracketsSet[0]);
+    closedBrackets.push(bracketsSet[1]);
+  }
+
+  let brackets = str.split("");
+  let removed = false;
+  
+  while (brackets.length > 0) {
+    for (let open = 0, closed = 1; closed < brackets.length ; open++, closed++) {
+      if ( openBrackets.indexOf(brackets[open]) === closedBrackets.indexOf(brackets[closed]) && openBrackets.indexOf(brackets[open]) !== -1) {
+        brackets.splice(open, 2);
+        removed = true;
+        break;
+      }
+    }
+
+    if (removed) {
+      removed = false;
+      continue;
+    }
+
+    return false;
+  }
+
+  return true
 }
